d524295f31ba43b44aeada73c5445f16e8ea4cc6
Collin Monahan <llaenowyd@gmail.com>
2021-06-26 14:43:39 -0400

    set font, remove bootstrap globals

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,16 @@ class MyDocument extends Document {
       <Html>
         <Head lang="en">
           <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600&display=swap"
+          />
         </Head>
         <body>
           <Main />
